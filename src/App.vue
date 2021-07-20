5d533b422f40f762a376020e08bc8500823a709f
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-07-20 19:48:26 +0300

    first looking to Vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/ships">Ships</router-link>
     </div>
     <router-view/>
   </div>
@@ -10,23 +10,50 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
 }
 
 #nav {
   padding: 30px;
+  margin-bottom: 20px;
+  width: 100vw;
 }
 
 #nav a {
+  font-size: 20px;
   font-weight: bold;
-  color: #2c3e50;
+  margin: 0 20px;
+  padding: 10px;
+  color: #42b983 ;
+  border: 1px solid #42b983;
+  border-radius: 4px;
+  transition: .3s;
+}
+
+#nav a:hover {
+  background: rgba(66, 185, 131, .5);
+  transition: .3s;
+}
+
+
+#nav a:active {
+  background: rgba(66, 185, 131, .8);
+  transition: .3s;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: antiquewhite;
+   background: rgba(66, 185, 131, .8);
 }
+
+
 </style>
---
+++ src/components/EnteringUniverse.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <code>
+      First try on Vue
+    </code>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EnteringUniverse",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+code {
+  font-size: 20px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
